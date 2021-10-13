7b029402d8729d8d37292b7f5c3b9712c8a24101
Eugene Gulyaev <baddman@list.ru>
2021-10-13 17:07:55 +0300

    Добавлены проверки

--- video.py
+++ video.py
@@ -6,11 +6,14 @@ async def videoScreen(host, user_id):
     filename = host.split(":")[0]
     username = user_id
 
-    endTime = datetime.datetime.now() + datetime.timedelta(seconds=1)
+    endTime = datetime.datetime.now() + datetime.timedelta(seconds=10)
     while True:
         ret, frame = captura.read()
-        if datetime.datetime.now() >= endTime:
-            cv2.imwrite(f"screens/{filename}-{username}.png", frame)
+        if ret == True:
+            if datetime.datetime.now() >= endTime:
+                cv2.imwrite(f"screens/{filename}-{username}.png", frame)
+                break
+        else:
             break
 
     captura.release()
