9cb2a5a107c942fae4abaedca06024858e5f0cd6
Eugene Gulyaev <baddman@list.ru>
2021-10-13 17:44:54 +0300

    Добавлена инструкция

--- video.py
+++ video.py
@@ -1,14 +1,15 @@
 import cv2, datetime
 
-async def videoScreen(host, user_id):
 
-    captura = cv2.VideoCapture(f'http://{host}/')
+async def videoScreen(host, user_id):
+    #TODO: Доделать проверку пинг
+    capture = cv2.VideoCapture(f'http://{host}/')
     filename = host.split(":")[0]
     username = user_id
 
     endTime = datetime.datetime.now() + datetime.timedelta(seconds=10)
     while True:
-        ret, frame = captura.read()
+        ret, frame = capture.read()
         if ret == True:
             if datetime.datetime.now() >= endTime:
                 cv2.imwrite(f"screens/{filename}-{username}.png", frame)
@@ -16,5 +17,4 @@ async def videoScreen(host, user_id):
         else:
             break
 
-    captura.release()
-    # cv2.destroyAllWindows()
+    capture.release()
