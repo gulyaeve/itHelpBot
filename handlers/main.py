6f09006cf474952f0206c08dc49984b43e510ef2
gulyaeve <gulyaev.e@icloud.com>
2022-02-15 16:00:46 +0300

    Добавлен хендлер для любого текста

--- handlers/main.py
+++ handlers/main.py
@@ -1,6 +1,7 @@
 from aiogram import types
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
+from logging import log, INFO
 
 from loader import dp
 
@@ -10,6 +11,7 @@ async def cmd_start(message: types.Message):
     """
     Conversation's entry point
     """
+    log(INFO, f"[{message.from_user.id}] нажал START.")
     await message.reply("Добро пожаловать в чат-бот техподдержки!")
 
 
@@ -21,5 +23,14 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     """
     Allow user to cancel any action
     """
+    log(INFO, f"[{message.from_user.id}] отменил действие.")
     await state.finish()
     await message.reply('Действие отменено.', reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(Text)
+async def text_handler(message: types.Message):
+    """
+    Any text handler
+    """
+    log(INFO, f"[{message.from_user.id}] написал: [{message.text}]")
