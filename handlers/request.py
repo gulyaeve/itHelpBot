5a0de817cc70e050828aae0c8ee06dbb66ced1ac
gulyaeve <gulyaev.e@icloud.com>
2022-02-19 07:39:43 +0300

    Перенос логирования в отправке запроса

--- handlers/request.py
+++ handlers/request.py
@@ -86,5 +86,5 @@ async def request_send(message: types.Message, state: FSMContext):
     if message.text == "Отправить":
         data = await state.get_data()
         answer = await send_request(data["id4me"], data["subject"], data["comment"], data["id_si"])
-        log(INFO, f"Пользователь [{message.from_user.id}] создал запрос [{answer['id']}]")
         await message.answer(f"Запрос успешно отправлен!")
+        log(INFO, f"Пользователь [{message.from_user.id}] создал запрос [{answer['id']}]")
