df7c27e7929a7fa2c2871a2af307a7a06a86f42d
gulyaeve <gulyaev.e@icloud.com>
2022-03-20 17:33:29 +0300

    Изменено логирование и логи добавлены в bind volume

--- loader.py
+++ loader.py
@@ -7,7 +7,7 @@ from config import telegram_token
 bot = Bot(token=telegram_token, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
-logging.basicConfig(handlers=(logging.FileHandler('log.txt'), logging.StreamHandler()),
-                    format=u'%(asctime)s %(filename)s [LINE:%(lineno)d] [%(funcName)s()] #%(levelname)-15s %(message)s',
+logging.basicConfig(handlers=(logging.FileHandler('logs/log.txt'), logging.StreamHandler()),
+                    format=u'%(asctime)s %(filename)s [LINE:%(lineno)d] #%(levelname)-15s %(message)s',
                     level=logging.INFO,
                     )
