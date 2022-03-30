23d76b40c2175d9dc635d221106cd294b537505a
gulyaeve <gulyaev.e@icloud.com>
2022-03-30 09:48:32 +0300

    Добавлен тест для базы данных

---
+++ utils/db_api/test_db.py
@@ -0,0 +1,39 @@
+import asyncio
+
+import config
+from utils.db_api.postgresql import Database
+
+
+async def test():
+    db = Database()
+    # await db.create()
+
+    # print("Создаем таблицу Пользователей...")
+    # await db.drop_users()
+    # await db.create_table_users()
+    # print("Готово")
+
+    # print("Добавляем пользователей")
+    # await db.add_user("EUGENE", "gulyaeve", 1234)
+    # user = await db.select_user(id=1234)
+    # print(user)
+
+    # await db.update_user_id4me(1234, 54321)
+    # user = await db.select_user(id=1234)
+    # print(user)
+
+    # await db.add_user("One", "onetwol", 123)
+    # await db.add_user("Vasya", "vv_alakaz", 1234)
+    # await db.add_user("1", "1", 131231)
+    # await db.add_user("1", "1", 23324234)
+    # await db.add_user("John", "JohnDoe", 4388229)
+    # print("Готово")
+
+    users = await db.select_all_users()
+    print(f"Получил всех пользователей: {users}")
+
+    # user = await db.select_user(id=5)
+    # print(f"Получил пользователя: {user}")
+
+
+asyncio.run(test())
