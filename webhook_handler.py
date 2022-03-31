db7654252aa99f495cd5ecb4a42a8132e45145dc
gulyaeve <gulyaev.e@icloud.com>
2022-03-31 09:53:58 +0300

    Webhook handler now on aiohttp

--- webhook_handler.py
+++ webhook_handler.py
@@ -1,13 +1,38 @@
+from logging import log, INFO
+
+import aiohttp
 from aiohttp import web
 
 routes = web.RouteTableDef()
 
 
-@routes.post('/requests')
+async def get_json(route):
+    """
+    Send get request to host
+    @param route: request link
+    @return: json object answer from host
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f'{route}', ssl=False) as resp:
+            return await resp.json()
+
+
+@routes.post('/request')
 async def hello(request):
-    data = await request.json()
-    print(dict(data))
-    return web.Response(text="Hello, world")
+    log(INFO, f"POST request: {request}")
+    print(f"POST request: {request}")
+    try:
+        log(INFO, f"Try to verify")
+        print(f"Try to verify")
+        data = await request.json()
+        callback = data["payload"]["callback"]
+        answer = await get_json(callback)
+        log(INFO, f"Answer to verify: {answer}")
+        print(f"Answer to verify: {answer}")
+    except:
+        log(INFO, f"WRONG POST request: {request}")
+        print(f"WRONG POST request: {request}")
+    return web.Response(text="{'message':'webhook.verify'}")
 
 
 app = web.Application()
