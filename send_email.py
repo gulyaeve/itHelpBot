9b99a9ac0e84a9f297d0a8323fd73e8ef8827e11
gulyaeve <gulyaev.e@icloud.com>
2022-04-14 15:16:06 +0300

    Переход на асинхронную отправку почты. aiosmtplib

--- send_email.py
+++ send_email.py
@@ -8,14 +8,23 @@ from config import sender_email, email_password, email_server, email_port, email
 
 async def send_email(recipient, message):
     try:
-        server = aiosmtplib.SMTP(email_server, email_port, timeout=1)
+        # server = aiosmtplib.SMTP(email_server, email_port, timeout=1)
         # smtplib.SMTP(email_server, email_port, timeout=1)
-        await server.starttls()
-        await server.login(email_login, email_password)
+        # await server.starttls()
+        # await server.login(email_login, email_password)
         msg = MIMEText(message)
         msg["Subject"] = "itHelpBot Authorization"
-        await server.sendmail(sender_email, recipient, msg.as_string())
+        await aiosmtplib.send(message=msg,
+                              sender=sender_email,
+                              recipients=recipient,
+                              hostname=email_server,
+                              port=email_port,
+                              username=email_login,
+                              password=email_password,
+                              timeout=1,
+                              start_tls=True)
+        # await server.sendmail(sender_email, recipient, msg.as_string())
         log(msg=f"Success email[{recipient}]: {message}", level=INFO)
-        await server.quit()
+        # await server.quit()
     except Exception as _ex:
         log(msg=f"{Exception}: {_ex}: Failed to send email[{recipient}]", level=INFO)
